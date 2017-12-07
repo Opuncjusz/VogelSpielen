01b1a6eb914e24e05857b7c4d6e17f5dd2b1b92c
Aleksander <aleksander.marciniak@capgemini.com>
2017-12-07 19:19:09 +0100

    Algorithm

--- src/main/java/service/impl/AlgorithmServiceImpl.java
+++ src/main/java/service/impl/AlgorithmServiceImpl.java
@@ -1,10 +1,19 @@
 package service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import service.AlgorithmService;
+import service.DesireService;
 
 @Service
 public class AlgorithmServiceImpl implements AlgorithmService {
 
+	@Autowired
+	private DesireService desireService;
+
+	@Override
+	public void calculate() {
+
+	}
 }
--- src/test/java/service/impl/AlgorithmServiceImplTest.java
+++ src/test/java/service/impl/AlgorithmServiceImplTest.java
@@ -1,13 +1,22 @@
 package service.impl;
 
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import service.AlgorithmService;
 import test.TestApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestApplicationContext.class)
 public class AlgorithmServiceImplTest {
+
+	@Autowired
+	private AlgorithmService algorithmService;
+
+	public void calculateTest() {
+
+	}
 
 }
