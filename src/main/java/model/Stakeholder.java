b14c716395d41d921b1eb860f6e0ab943f30d373
Aleksander <aleksander.marciniak@capgemini.com>
2017-12-07 19:35:37 +0100

    fix3

--- src/main/java/model/Stakeholder.java
+++ src/main/java/model/Stakeholder.java
@@ -4,21 +4,11 @@ public class Stakeholder {
 
 	private static long sequence = 0;
 
-	private String id;
 	private String clientMobileToken;
 
 	public Stakeholder(String clientMobileToken) {
 		this.clientMobileToken = clientMobileToken;
-		this.id = sequence + "";
 		sequence++;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
 	}
 
 	public String getClientMobileToken() {
@@ -33,29 +23,24 @@ public class Stakeholder {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((clientMobileToken == null) ? 0 : clientMobileToken.hashCode());
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (obj == null)
 			return false;
-		}
-		if (getClass() != obj.getClass()) {
+		if (getClass() != obj.getClass())
 			return false;
-		}
 		Stakeholder other = (Stakeholder) obj;
-		if (id == null) {
-			if (other.id != null) {
+		if (clientMobileToken == null) {
+			if (other.clientMobileToken != null)
 				return false;
-			}
-		} else if (!id.equals(other.id)) {
+		} else if (!clientMobileToken.equals(other.clientMobileToken))
 			return false;
-		}
 		return true;
 	}
 
