94ad9346b4da5f034b03c78df51898d0bd056435
Aleksander <aleksander.marciniak@capgemini.com>
2017-12-07 12:26:37 +0100

    Now we can handle request content

--- src/main/java/frontend/MobileClientController.java
+++ src/main/java/frontend/MobileClientController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,9 +24,10 @@ public class MobileClientController {
 	// private FinderService finderService;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public CommonTO getStatusPOST(@PathVariable("userId") String userId) {
+	public CommonTO getStatusPOST(@PathVariable("userId") String userId, @RequestBody String requestBody) {
 
 		LOG.info("userId = " + userId);
+		LOG.info("requestBody = " + requestBody);
 
 		// long status = finderService.getStatus();
 		CommonTO commonTO = createCommonTO(0);
@@ -35,9 +37,9 @@ public class MobileClientController {
 
 	@RequestMapping(method = RequestMethod.GET, value = "/user/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public CommonTO getStatusGET(@PathVariable("userId") String userId) {
-		CommonTO status = getStatusPOST(userId);
+		CommonTO status = getStatusPOST(userId, "[RequestMethod.GET]");
 		status.setMessage("ONLY POST METHOD IST AVAILABLE");
-		return getStatusPOST(userId);
+		return status;
 	}
 
 	// @RequestMapping(method = RequestMethod.GET, value = "/user/{userId}",
