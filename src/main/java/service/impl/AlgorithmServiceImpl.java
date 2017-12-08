0c90909ca091bf8c0b0f515b82e8f98e29197c4f
Aleksander <aleksander.marciniak@capgemini.com>
2017-12-08 07:04:22 +0100

    ShipItDay

--- src/main/java/service/impl/AlgorithmServiceImpl.java
+++ src/main/java/service/impl/AlgorithmServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import common.AlgorithmResultDS;
 import common.DesireTO;
@@ -15,54 +16,66 @@ import service.DesireService;
 @Service
 public class AlgorithmServiceImpl implements AlgorithmService {
 
-    @Autowired
-    private DesireService desireService;
+	@Autowired
+	private DesireService desireService;
 
-    @Override
-    public void calculate() {
+	@Override
+	public void calculate() {
 
-    }
+	}
 
-    @Override
-    public AlgorithmResultDS calculateBasedOnIncoming(DesireTO incomingDesire) {
+	@Override
+	public AlgorithmResultDS calculateBasedOnIncoming(DesireTO incomingDesire) {
 
-        AlgorithmResultDS algorithmResult = new AlgorithmResultDS();
+		AlgorithmResultDS algorithmResult = new AlgorithmResultDS();
 
-        List<DesireTO> allDesires = desireService.getAllDesires();
+		List<DesireTO> allDesires = desireService.getAllDesires();
 
-        for (DesireTO currentDesire : allDesires) {
-            if (checkStakeholderIsDifferent(currentDesire, incomingDesire)
-                    && checkTimeWindowExists(currentDesire, incomingDesire)
-                    && checkRequiredPeopleEnoughOrLess(currentDesire, incomingDesire)
-                    && checkPlaceMatches(currentDesire, incomingDesire)) {
+		for (DesireTO currentDesire : allDesires) {
+			if (checkStakeholderIsDifferent(currentDesire, incomingDesire)
+					&& checkTimeWindowExists(currentDesire, incomingDesire)
+					&& checkRequiredPeopleEnoughOrLess(currentDesire, incomingDesire)
+					&& checkPlaceMatches(currentDesire, incomingDesire)
+					&& checkTotalEqual(currentDesire, incomingDesire)) {
 
-                MatchTO match = new MatchTO();
-                match.setDesires(new ArrayList());
-                match.getDesires().add(incomingDesire);
-                match.getDesires().add(currentDesire);
-                match.setPlace(incomingDesire.getPlace());
+				MatchTO match = new MatchTO();
+				match.setDesires(new ArrayList());
+				match.getDesires().add(incomingDesire);
+				match.getDesires().add(currentDesire);
+				match.setPlace(incomingDesire.getPlace());
 
-                algorithmResult.setCreatedMatch(match);
-                return algorithmResult;
-            }
-        }
+				algorithmResult.setCreatedMatch(match);
+				return algorithmResult;
+			}
+		}
 
-        return algorithmResult;
-    }
+		return algorithmResult;
+	}
 
-    private boolean checkStakeholderIsDifferent(DesireTO desire1, DesireTO desire2) {
-        return !desire1.getStakeholder().equals(desire2.getStakeholder());
-    }
+	private boolean checkStakeholderIsDifferent(DesireTO desire1, DesireTO desire2) {
+		return !desire1.getStakeholder().equals(desire2.getStakeholder());
+	}
 
-    private boolean checkTimeWindowExists(DesireTO desire1, DesireTO desire2) {
-        return true;
-    }
+	private boolean checkTimeWindowExists(DesireTO desire1, DesireTO desire2) {
+		return true;
+	}
 
-    private boolean checkRequiredPeopleEnoughOrLess(DesireTO desire1, DesireTO desire2) {
-        return true;
-    }
+	private boolean checkTotalEqual(DesireTO desire1, DesireTO desire2) {
+		String total1 = desire1.getTotal();
+		String total2 = desire2.getTotal();
 
-    private boolean checkPlaceMatches(DesireTO desire1, DesireTO desire2) {
-        return desire1.getPlace().equals(desire2.getPlace());
-    }
+		if (StringUtils.isEmpty(total1)) {
+			total1 = "";
+		}
+
+		return total1.equals(total2);
+	}
+
+	private boolean checkRequiredPeopleEnoughOrLess(DesireTO desire1, DesireTO desire2) {
+		return true;
+	}
+
+	private boolean checkPlaceMatches(DesireTO desire1, DesireTO desire2) {
+		return desire1.getPlace().equals(desire2.getPlace());
+	}
 }
--- src/test/java/service/impl/AlgorithmServiceImplTest.java
+++ src/test/java/service/impl/AlgorithmServiceImplTest.java
@@ -21,235 +21,235 @@ import test.TestApplicationContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class AlgorithmServiceImplTest {
 
-    @Autowired
-    private AlgorithmService algorithmService;
+	@Autowired
+	private AlgorithmService algorithmService;
 
-    @Autowired
-    private DesireService desireService;
+	@Autowired
+	private DesireService desireService;
 
-    private DesireTO createDefaultFirstDesire() {
-        return createDefaultDesire(1, new Stakeholder("Dupcio 1"));
-    }
+	private DesireTO createDefaultFirstDesire() {
+		return createDefaultDesire(1, new Stakeholder("Dupcio 1"));
+	}
 
-    private DesireTO createDefaultSecondDesire() {
-        return createDefaultDesire(2, new Stakeholder("Dupcio 2"));
-    }
+	private DesireTO createDefaultSecondDesire() {
+		return createDefaultDesire(2, new Stakeholder("Dupcio 2"));
+	}
 
-    private DesireTO createDefaultThirdDesire() {
-        return createDefaultDesire(3, new Stakeholder("Dupcio 3"));
-    }
+	private DesireTO createDefaultThirdDesire() {
+		return createDefaultDesire(3, new Stakeholder("Dupcio 3"));
+	}
 
-    private DesireTO createDefaultDesire(long id, Stakeholder secondStakeholder) {
-        DesireTO secondDesire = new DesireTO();
-        secondDesire.setId(id);
-        secondDesire.setPlace("miejsce");
-        secondDesire.setFrom("2017-12-07 15:00");
-        secondDesire.setTo("2017-12-07 16:00");
-        secondDesire.setTotal("4");
-        secondDesire.setRequired("2");
-        secondDesire.setStakeholder(secondStakeholder);
-        return secondDesire;
-    }
+	private DesireTO createDefaultDesire(long id, Stakeholder secondStakeholder) {
+		DesireTO secondDesire = new DesireTO();
+		secondDesire.setId(id);
+		secondDesire.setPlace("miejsce");
+		secondDesire.setFrom("2017-12-07 15:00");
+		secondDesire.setTo("2017-12-07 16:00");
+		secondDesire.setTotal("4");
+		secondDesire.setRequired("2");
+		secondDesire.setStakeholder(secondStakeholder);
+		return secondDesire;
+	}
 
-    @Test
-    public void shouldNotMatchOnDifferentPlace() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchOnDifferentPlace() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        firstDesire.setPlace("inne");
-        secondDesire.setPlace("miejsce");
+		firstDesire.setPlace("inne");
+		secondDesire.setPlace("miejsce");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 
-    @Test
-    public void shouldNotMatchOnTheSameStakeholder() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchOnTheSameStakeholder() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        firstDesire.setStakeholder(secondDesire.getStakeholder());
+		firstDesire.setStakeholder(secondDesire.getStakeholder());
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 
-    @Test
-    public void shouldNotMatchOnDifferentTimesTooEarly() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchOnDifferentTimesTooEarly() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        firstDesire.setFrom("2017-12-07 14:00");
-        firstDesire.setTo("2017-12-07 15:00");
-        secondDesire.setFrom("2017-12-07 15:00");
-        secondDesire.setTo("2017-12-07 16:00");
+		firstDesire.setFrom("2017-12-07 14:00");
+		firstDesire.setTo("2017-12-07 15:00");
+		secondDesire.setFrom("2017-12-07 15:00");
+		secondDesire.setTo("2017-12-07 16:00");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 
-    @Test
-    public void shouldNotMatchOnDifferentTimesTooLate() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchOnDifferentTimesTooLate() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        firstDesire.setFrom("2017-12-07 15:00");
-        firstDesire.setTo("2017-12-07 16:00");
-        secondDesire.setFrom("2017-12-07 14:00");
-        secondDesire.setTo("2017-12-07 15:00");
+		firstDesire.setFrom("2017-12-07 15:00");
+		firstDesire.setTo("2017-12-07 16:00");
+		secondDesire.setFrom("2017-12-07 14:00");
+		secondDesire.setTo("2017-12-07 15:00");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 
-    @Test
-    public void shouldNotMatchOnNotEnoughPeople() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchOnNotEnoughPeople() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        firstDesire.setTotal("4");
-        firstDesire.setRequired("2");
-        secondDesire.setTotal("4");
-        secondDesire.setRequired("3");
+		firstDesire.setTotal("4");
+		firstDesire.setRequired("2");
+		secondDesire.setTotal("4");
+		secondDesire.setRequired("3");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 
-    @Test
-    public void shouldNotMatchOnTooMuchPeople() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchOnTooMuchPeople() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        firstDesire.setTotal("4");
-        firstDesire.setRequired("2");
-        secondDesire.setTotal("4");
-        secondDesire.setRequired("1");
+		firstDesire.setTotal("4");
+		firstDesire.setRequired("2");
+		secondDesire.setTotal("4");
+		secondDesire.setRequired("1");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 
-    @Test
-    public void shouldMatchTwoDesires() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldMatchTwoDesires() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(secondDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNotNull(match);
-        Assert.assertEquals(match.getPlace(), "miejsce");
-        Assert.assertEquals(match.getDesires().size(), 2);
-        Assert.assertTrue(match.getDesires().contains(firstDesire));
-        Assert.assertTrue(match.getDesires().contains(secondDesire));
-        // TODO: require proper "from" and "to" based on business logic
-    }
+		Assert.assertNotNull(match);
+		Assert.assertEquals(match.getPlace(), "miejsce");
+		Assert.assertEquals(match.getDesires().size(), 2);
+		Assert.assertTrue(match.getDesires().contains(firstDesire));
+		Assert.assertTrue(match.getDesires().contains(secondDesire));
+		// TODO: require proper "from" and "to" based on business logic
+	}
 
-    @Test
-    public void shouldMatchThreeDesires() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
-        DesireTO thirdDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldMatchThreeDesires() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
+		DesireTO thirdDesire = createDefaultSecondDesire();
 
-        firstDesire.setTotal("4");
-        secondDesire.setTotal("4");
-        thirdDesire.setTotal("4");
+		firstDesire.setTotal("4");
+		secondDesire.setTotal("4");
+		thirdDesire.setTotal("4");
 
-        firstDesire.setRequired("2");
-        secondDesire.setRequired("1");
-        thirdDesire.setRequired("1");
+		firstDesire.setRequired("2");
+		secondDesire.setRequired("3");
+		thirdDesire.setRequired("3");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
-        desireService.handleIncomingDesire(secondDesire.getStakeholder(), secondDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(secondDesire.getStakeholder(), secondDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(thirdDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(thirdDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNotNull(match);
-        Assert.assertEquals(match.getPlace(), "miejsce");
-        Assert.assertEquals(match.getDesires().size(), 3);
-        Assert.assertTrue(match.getDesires().contains(firstDesire));
-        Assert.assertTrue(match.getDesires().contains(secondDesire));
-        Assert.assertTrue(match.getDesires().contains(thirdDesire));
-        // TODO: require proper "from" and "to" based on business logic
-    }
+		Assert.assertNotNull(match);
+		Assert.assertEquals(match.getPlace(), "miejsce");
+		Assert.assertEquals(match.getDesires().size(), 3);
+		Assert.assertTrue(match.getDesires().contains(firstDesire));
+		Assert.assertTrue(match.getDesires().contains(secondDesire));
+		Assert.assertTrue(match.getDesires().contains(thirdDesire));
+		// TODO: require proper "from" and "to" based on business logic
+	}
 
-    @Test
-    public void shouldMatchTwoOutOfThreeDesires() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
-        DesireTO thirdDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldMatchTwoOutOfThreeDesires() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
+		DesireTO thirdDesire = createDefaultSecondDesire();
 
-        firstDesire.setTotal("4");
-        secondDesire.setTotal("4");
-        thirdDesire.setTotal("4");
+		firstDesire.setTotal("4");
+		secondDesire.setTotal("4");
+		thirdDesire.setTotal("4");
 
-        firstDesire.setRequired("2");
-        secondDesire.setRequired("1");
-        thirdDesire.setRequired("2");
+		firstDesire.setRequired("2");
+		secondDesire.setRequired("1");
+		thirdDesire.setRequired("2");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
-        desireService.handleIncomingDesire(secondDesire.getStakeholder(), secondDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(secondDesire.getStakeholder(), secondDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(thirdDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(thirdDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNotNull(match);
-        Assert.assertEquals(match.getPlace(), "miejsce");
-        Assert.assertEquals(match.getDesires().size(), 2);
-        Assert.assertTrue(match.getDesires().contains(firstDesire));
-        Assert.assertFalse(match.getDesires().contains(secondDesire));
-        Assert.assertTrue(match.getDesires().contains(thirdDesire));
-        // TODO: require proper "from" and "to" based on business logic
-    }
+		Assert.assertNotNull(match);
+		Assert.assertEquals(match.getPlace(), "miejsce");
+		Assert.assertEquals(match.getDesires().size(), 2);
+		Assert.assertTrue(match.getDesires().contains(firstDesire));
+		Assert.assertFalse(match.getDesires().contains(secondDesire));
+		Assert.assertTrue(match.getDesires().contains(thirdDesire));
+		// TODO: require proper "from" and "to" based on business logic
+	}
 
-    @Test
-    public void shouldNotMatchThreeDesires() {
-        DesireTO firstDesire = createDefaultFirstDesire();
-        DesireTO secondDesire = createDefaultSecondDesire();
-        DesireTO thirdDesire = createDefaultSecondDesire();
+	@Test
+	public void shouldNotMatchThreeDesires() {
+		DesireTO firstDesire = createDefaultFirstDesire();
+		DesireTO secondDesire = createDefaultSecondDesire();
+		DesireTO thirdDesire = createDefaultSecondDesire();
 
-        firstDesire.setTotal("4");
-        secondDesire.setTotal("4");
-        thirdDesire.setTotal("4");
+		firstDesire.setTotal("4");
+		secondDesire.setTotal("4");
+		thirdDesire.setTotal("4");
 
-        firstDesire.setRequired("1");
-        secondDesire.setRequired("1");
-        thirdDesire.setRequired("1");
+		firstDesire.setRequired("1");
+		secondDesire.setRequired("1");
+		thirdDesire.setRequired("1");
 
-        desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
-        desireService.handleIncomingDesire(secondDesire.getStakeholder(), secondDesire);
+		desireService.handleIncomingDesire(firstDesire.getStakeholder(), firstDesire);
+		desireService.handleIncomingDesire(secondDesire.getStakeholder(), secondDesire);
 
-        AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(thirdDesire);
-        MatchTO match = algorithmResult.getCreatedMatch();
+		AlgorithmResultDS algorithmResult = algorithmService.calculateBasedOnIncoming(thirdDesire);
+		MatchTO match = algorithmResult.getCreatedMatch();
 
-        Assert.assertNull(match);
-    }
+		Assert.assertNull(match);
+	}
 }
