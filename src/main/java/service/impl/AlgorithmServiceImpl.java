cb5135f39447e19f6723d95b0bd655a7254c9de2
Opuncjusz <ARMOJSAK@CE18331.corp.capgemini.com>
2017-12-08 08:13:42 +0100

    Algorithm

--- src/main/java/service/impl/AlgorithmServiceImpl.java
+++ src/main/java/service/impl/AlgorithmServiceImpl.java
@@ -3,9 +3,12 @@ package service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import common.AlgorithmResultDS;
 import common.DesireTO;
@@ -16,66 +19,78 @@ import service.DesireService;
 @Service
 public class AlgorithmServiceImpl implements AlgorithmService {
 
-	@Autowired
-	private DesireService desireService;
+    public static DateTimeFormatter DEFAULT_DATE_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm");;
 
-	@Override
-	public void calculate() {
+    @Autowired
+    private DesireService desireService;
 
-	}
+    @Override
+    public void calculate() {
 
-	@Override
-	public AlgorithmResultDS calculateBasedOnIncoming(DesireTO incomingDesire) {
+    }
 
-		AlgorithmResultDS algorithmResult = new AlgorithmResultDS();
+    @Override
+    public AlgorithmResultDS calculateBasedOnIncoming(DesireTO incomingDesire) {
 
-		List<DesireTO> allDesires = desireService.getAllDesires();
+        AlgorithmResultDS algorithmResult = new AlgorithmResultDS();
 
-		for (DesireTO currentDesire : allDesires) {
-			if (checkStakeholderIsDifferent(currentDesire, incomingDesire)
-					&& checkTimeWindowExists(currentDesire, incomingDesire)
-					&& checkRequiredPeopleEnoughOrLess(currentDesire, incomingDesire)
-					&& checkPlaceMatches(currentDesire, incomingDesire)
-					&& checkTotalEqual(currentDesire, incomingDesire)) {
+        List<DesireTO> allDesires = desireService.getAllDesires();
 
-				MatchTO match = new MatchTO();
-				match.setDesires(new ArrayList());
-				match.getDesires().add(incomingDesire);
-				match.getDesires().add(currentDesire);
-				match.setPlace(incomingDesire.getPlace());
+        for (DesireTO currentDesire : allDesires) {
+            if (checkStakeholderIsDifferent(currentDesire, incomingDesire)
+                    && checkTimeWindowExists(currentDesire, incomingDesire)
+                    && checkRequiredPeopleEnoughOrLess(currentDesire, incomingDesire)
+                    && checkPlaceMatches(currentDesire, incomingDesire)
+                    && checkTotalsEqual(currentDesire, incomingDesire)
+                    && checkRequiredPeopleMeetsTotal(currentDesire, incomingDesire)) {
 
-				algorithmResult.setCreatedMatch(match);
-				return algorithmResult;
-			}
-		}
+                MatchTO match = new MatchTO();
+                match.setDesires(new ArrayList());
+                match.getDesires().add(incomingDesire);
+                match.getDesires().add(currentDesire);
+                match.setPlace(incomingDesire.getPlace());
 
-		return algorithmResult;
-	}
+                algorithmResult.setCreatedMatch(match);
+                return algorithmResult;
+            }
+        }
 
-	private boolean checkStakeholderIsDifferent(DesireTO desire1, DesireTO desire2) {
-		return !desire1.getStakeholder().equals(desire2.getStakeholder());
-	}
+        return algorithmResult;
+    }
 
-	private boolean checkTimeWindowExists(DesireTO desire1, DesireTO desire2) {
-		return true;
-	}
+    private boolean checkStakeholderIsDifferent(DesireTO desire1, DesireTO desire2) {
+        return !desire1.getStakeholder().equals(desire2.getStakeholder());
+    }
 
-	private boolean checkTotalEqual(DesireTO desire1, DesireTO desire2) {
-		String total1 = desire1.getTotal();
-		String total2 = desire2.getTotal();
+    private boolean checkTimeWindowExists(DesireTO desire1, DesireTO desire2) {
+        Interval firstInterval = new Interval(DateTime.parse(desire1.getFrom(), DEFAULT_DATE_FORMAT), DateTime.parse(desire1.getTo(), DEFAULT_DATE_FORMAT));
+        Interval secondInterval = new Interval(DateTime.parse(desire2.getFrom(), DEFAULT_DATE_FORMAT), DateTime.parse(desire2.getTo(), DEFAULT_DATE_FORMAT));
+        return firstInterval.overlaps(secondInterval);
+    }
 
-		if (StringUtils.isEmpty(total1)) {
-			total1 = "";
-		}
+    private boolean checkTotalsEqual(DesireTO desire1, DesireTO desire2) {
+        String total1 = desire1.getTotal();
+        String total2 = desire2.getTotal();
 
-		return total1.equals(total2);
-	}
+        if (total1 == null) {
+            total1 = "";
+        }
 
-	private boolean checkRequiredPeopleEnoughOrLess(DesireTO desire1, DesireTO desire2) {
-		return true;
-	}
+        return total1.equals(total2);
+    }
 
-	private boolean checkPlaceMatches(DesireTO desire1, DesireTO desire2) {
-		return desire1.getPlace().equals(desire2.getPlace());
-	}
+    private boolean checkRequiredPeopleEnoughOrLess(DesireTO desire1, DesireTO desire2) {
+        return true;
+    }
+
+    private boolean checkPlaceMatches(DesireTO desire1, DesireTO desire2) {
+        return desire1.getPlace().equals(desire2.getPlace());
+    }
+
+    private boolean checkRequiredPeopleMeetsTotal(DesireTO desire1, DesireTO desire2) {
+        int total = Integer.parseInt(desire1.getTotal());
+        int firstRequired = Integer.parseInt(desire1.getRequired());
+        int secondRequired = Integer.parseInt(desire2.getRequired());
+        return (firstRequired + secondRequired) == total;
+    }
 }
