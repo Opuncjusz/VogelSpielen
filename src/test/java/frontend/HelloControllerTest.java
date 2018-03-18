1449535147e4716402d0e82cd7e54db218fccae8
Aleksander <aleksander.marciniak@capgemini.com>
2018-03-18 13:36:40 +0100

    hallu, wie gehts - test

--- src/test/java/frontend/HelloControllerTest.java
+++ src/test/java/frontend/HelloControllerTest.java
@@ -1,33 +1,71 @@
-//package frontend;
+// package frontend;
 //
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+// import static
+// org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 //
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+// import org.junit.Assert;
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import
+// org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.http.MediaType;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.MvcResult;
+// import
+// org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 //
-//import test.TestApplicationContext;
+// import common.AnswerTO;
+// import test.TestApplicationContext;
 //
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = TestApplicationContext.class)
-//@AutoConfigureMockMvc
-//public class HelloControllerTest {
+// @RunWith(SpringRunner.class)
+// @SpringBootTest(classes = TestApplicationContext.class)
+// @AutoConfigureMockMvc
+// public class HelloControllerTest {
 //
-//    @Autowired
-//    private MockMvc mvc;
+// @Autowired
+// private MockMvc mvc;
 //
-//    @Test
-//    public void getHello() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-//    }
-//}
+// @Test
+// public void areServicesAvailable() {
+// try {
+// mvc.perform(MockMvcRequestBuilders.post("/status/" +
+// System.currentTimeMillis())
+// .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+// } catch (Exception e) {
+// Assert.fail(e.getMessage());
+// }
+//
+// try {
+// mvc.perform(MockMvcRequestBuilders.post("/desire/put/" +
+// System.currentTimeMillis())
+// .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+// } catch (Exception e) {
+// Assert.fail(e.getMessage());
+// }
+//
+// try {
+// mvc.perform(MockMvcRequestBuilders.post("/desire/delete/" +
+// System.currentTimeMillis())
+// .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+// } catch (Exception e) {
+// Assert.fail(e.getMessage());
+// }
+// }
+//
+// @Test
+// public void getHello() throws Exception {
+// MockHttpServletRequestBuilder mockHttpServletRequestBuilder =
+// MockMvcRequestBuilders.get("/status/123");
+//
+// MvcResult mvcResult = mvc.perform(mockHttpServletRequestBuilder).andReturn();
+//
+// AnswerTO answerTO = (AnswerTO)
+// mvcResult.getModelAndView().getModel().get("answerTO");
+//
+// System.out.println("answerTO = " + answerTO);
+// }
+// }
